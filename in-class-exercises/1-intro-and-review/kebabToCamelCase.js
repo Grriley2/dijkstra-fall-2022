453d00ffb2e50a0525e096337c9fc1a609561d29
Grriley2 <107074562+Grriley2@users.noreply.github.com>
2022-10-14 07:41:08 -0400

    Updated function

--- in-class-exercises/1-intro-and-review/kebabToCamelCase.js
+++ in-class-exercises/1-intro-and-review/kebabToCamelCase.js
@@ -1,25 +1,28 @@
-// Convert kebab case works to camel casing. 
-// The first word within the output should be capitalized only if it is the first letter of a word and dashes should be removed.
-
-// change-this-word gets converted into changeThisWord
-// day-moth-year gets converted into dayMonthYear
-// ALL-CAPS gets converted into allCaps
-
-var testString = "hello-world"
 function kebabToCamel (kebabString) {
-   let array = [];
-  let camelString = array.push(kebabString)
-   for (let i=0; i<=kebabString.length; i++) {
-     if (kebabString[i] === "-" )
-       kebabString.substr(i) && kebabString.toUpperCase(i++)
-                                                                          
+    var kebabSplit = kebabString.split("-")
+    var newStringCamel = "" 
+   
+    for (i = 0; i < kebabSplit.length; i++) {
+      let firstLetter = kebabSplit[i].substr(0,1).toUpperCase()
+      let restOfWord = kebabSplit[i].substr(1).toLowerCase()
+       
+      newStringCamel += (firstLetter + restOfWord)
+    }
+     // let firstLetter = newStringCamel.substr(0,1).toLowerCase()
+     // let restOfWord = newStringCamel.substr(1)
+     var camelCaseWord = newStringCamel.substr(0,1).toLowerCase() + newStringCamel.substr(1)
+     
+     
+     // return (firstLetter + restOfWord) 
+   return camelCaseWord
+    
    }
- console.log(kebabString)
-  console.log(camelString)
- 
-   return camelString;
-  
-}
-   console.log(kebabToCamel(testString))
+   console.log(kebabToCamel("heLLO-WORLD"))
+   
+       //loop over kebabstring
+       // - if the char dash, don't use it AND the following char would be capitalized
+       // - if char [1] is cap, change to lowercase
+       // -otherwise, copy the lowercased to camelstring
+       
 
 }
